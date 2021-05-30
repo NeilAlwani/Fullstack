c229cb8f5160c886d8370454494a0b12e6f6ee3b
nalwani <N.M.Alwani@student.tudelft.nl>
2021-05-30 11:02:30 +0200

    Restructuring files and added parts read me

---
+++ Parts/Parts.md
@@ -0,0 +1,11 @@
+# Full Stack Open 2021
+
+Learning React, Redux, Node.js, MongoDB, GraphQL and TypeScript in one go! Find the course [here](https://fullstackopen.com/en/).
+
+### Part 1
+
+In this part, I got familiarized with the React-library. Also, I got a look at some features of JavaScript that are important for understanding React.
+
+### Part 2
+
+In this part, I familiarized myself with how to render a data collection, data submition to a React application using HTML forms. Also, I got a look at how to fetch and handle data stored in a remote backend server.
---
+++ Parts/part1.js
@@ -0,0 +1,120 @@
+/**
+ * 1.b Unicafe Feedback Collector
+ */
+
+// const App = () =>{
+//   const [Good, setGood] = useState(0)
+//   const [Neutral, setNeutral] = useState(0)
+//   const [Bad, setBad] = useState(0)
+  
+//   const handleGood = () => {
+//     setGood(Good + 1)
+//   }
+
+//   const handleNeutral = () => {
+//     setNeutral(Neutral + 1)
+//   }
+
+//   const handleBad = () => {
+//     setBad(Bad + 1)
+//   }
+
+//   const all = Good + Bad + Neutral
+  
+//   const avgFeedback =  (Good - Bad) / all
+    
+  
+//   return(
+//     <div>
+//       <h1>Give feedback</h1>
+//       <Button 
+//       handleClick={handleGood} text={'good'} />
+//       <Button handleClick= {handleNeutral} text={'neutral'} />
+//       <Button handleClick={handleBad} text={'bad'} />
+//       <h1>Stats </h1>
+//       <History good={Good} nu={Neutral} ba={Bad} all={all} avg={avgFeedback}/>
+//     </div>
+//   )
+// }
+
+
+/**
+ * This is part of 1.b of the full stack course.
+ */
+
+// const App = () => {
+//   const course = {
+//     name:  'Half Stack application develpment',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         ex: 10
+//       },
+//       { 
+//         name: 'Using props to pass data',
+//         ex: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         ex: 14
+//       } 
+//     ]
+//   }
+
+  
+//   return(
+//   <div>
+//     <Header course ={course.name}></Header>
+//     <Content parts = {course.parts}></Content>
+//     <Total parts={course.parts}></Total>
+    
+//   </div>
+//   ) 
+// }
+
+/**
+ * This is part of 1.a
+ */
+
+// History component
+// const History = (props) => {
+//   if (props.allClick.length == 0) {
+//     return(
+//       <div>
+//         the app is used be pressing the buttons
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       button press history: {props.allClick.join(' ')}
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [clicks, setClicks] = useState({left: 0, right:0})
+//   const [allClick, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClick.concat('L'))
+//     setClicks({ left: clicks.left + 1, right: clicks.right})
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClick.concat('R'))
+//     setClicks({left: clicks.left, right: clicks.right + 1})
+//   }
+
+ 
+//   return(
+//     <div>
+//       {clicks.left}
+//       <Button handleClick={handleLeftClick} text={"Left"} />
+//       <Button handleClick={handleRightClick} text={"Right"} />
+//       {clicks.right}
+//       <History allClick={allClick}/>
+//     </div>
+//   )
+// }
